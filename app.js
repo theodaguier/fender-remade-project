8c5957de5e9aa7fe845edc6fa288fb39ba7dc616
theodaguier <92515854+theodaguier@users.noreply.github.com>
2022-07-20 01:15:28 +0200

    update

--- app.js
+++ app.js
@@ -3,16 +3,19 @@
 let toggleBtn = document.querySelector('.toggle');
 let openBtn = document.querySelector('.openBtn');
 let closeBtn = document.querySelector('.closeBtn');
+let menu = document.querySelector('.menu--mobile--container')
 
 function toggleMenu () {
     if (toggleBtn.classList.contains('noactive')) {
         toggleBtn.classList.remove('noactive');
         openBtn.style.display = "none";
         closeBtn.style.display = "block";
+        menu.style.display = "block";
     }else {
         toggleBtn.classList.add('noactive');
         openBtn.style.display = "block";
         closeBtn.style.display = "none";
+        menu.style.display = "none";
     }
 }
 
