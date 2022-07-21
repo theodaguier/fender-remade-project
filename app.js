8511f4602e0dc93f741517055b1d466763a1afdc
theodaguier <92515854+theodaguier@users.noreply.github.com>
2022-07-21 20:14:56 +0200

    Update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Hamburger Menu 
 
+let body = document.querySelector('body')
 let toggleBtn = document.querySelector('.toggle');
 let openBtn = document.querySelector('.openBtn');
 let closeBtn = document.querySelector('.closeBtn');
@@ -11,11 +12,13 @@ function toggleMenu () {
         openBtn.style.display = "none";
         closeBtn.style.display = "block";
         menu.style.display = "block";
+        body.style.overflowY = "hidden"
     }else {
         toggleBtn.classList.add('noactive');
         openBtn.style.display = "block";
         closeBtn.style.display = "none";
         menu.style.display = "none";
+        body.style.overflowY = "auto"
     }
 }
 
