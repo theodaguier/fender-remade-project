249c585777e7574936ceb2e7a72c9772a4738885
theodaguier <92515854+theodaguier@users.noreply.github.com>
2022-07-22 19:04:40 +0200

    Update

---
+++ javascript/gsap-anim.js
@@ -0,0 +1,9 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+// Shop 
+gsap.from(".news", { duration: 1.5, y: 50});
+
+//Product Page
+gsap.from(".product-p-container", { duration: 1.5, y: 50});
+gsap.from(".f-img", { opacity: 0, duration: 2, x: 550});
